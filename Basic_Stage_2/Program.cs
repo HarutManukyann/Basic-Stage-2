b32f98658a0e7d1cd8db0c42cc234d537adfcefc
harut.manukyan.g@gmail.com <harut.manukyan.g@gmail.com>
2022-08-10 15:12:52 +0400

    Add project files.

---
+++ Basic_Stage_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Stage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
